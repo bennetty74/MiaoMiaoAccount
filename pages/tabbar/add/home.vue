6833c45d5e2fb31a7a10ca3dcc0aac26400af24e
bennetty74 <bennetty74@gmail.com>
2020-10-13 21:27:33 +0800

    修改键盘

--- pages/tabbar/add/home.vue
+++ pages/tabbar/add/home.vue
@@ -50,7 +50,10 @@
 			<view class="value">
 				<!-- <image class="icon" src="../../../static/img/tabbar/add.png" mode=""></image> -->
 				<view class="text">
-					微信零钱
+					<!-- 微信零钱 -->
+					<picker @change="bindAccountChange" :value="index" :range="accounts">
+						<view class="uni-input">{{accounts[index]}}</view>
+					</picker>
 				</view>
 			</view>
 		</view>
@@ -59,9 +62,8 @@
 			<view class="keybord-top">
 				<view class="note">
 					<image class="icon" src="../../../static/img/qa.png" mode=""></image>
-					<text class="text">备注:</text>
+					<text class="text">金额：</text>
 				</view>
-				<input class="note-input" type="text" v-model="note" placeholder="填写备注..."/>
 				<view class="amount">
 					{{amount}}
 				</view>
@@ -70,7 +72,7 @@
 				<view class="item number" data-num="7"  @click="getValue($event)">7</view>
 				<view class="item number" data-num="8"  @click="getValue($event)">8</view>
 				<view class="item number" data-num="9"  @click="getValue($event)">9</view>
-				<view class="item number" @click="backspace()">Del</view>
+				<view class="item number" @click="backspace()"><image src="../../../static/img/add/backspace.png" class="icon"></image></view>
 				
 				<view class="item number" data-num="4"  @click="getValue($event)">4</view>
 				<view class="item number" data-num="5"  @click="getValue($event)">5</view>
@@ -84,7 +86,7 @@
 				
 				<view class="item number" data-num="."  @click="getValue($event)">.</view>
 				<view class="item number" data-num="0"  @click="getValue($event)">0</view>
-				<view class="item number" data-num="="  @click="getValue($event)">=</view>
+				<view class="item number" data-num="="  @click="getEquals()">=</view>
 				<view class="item complete" @click="submit()">完成</view>
 			</view>
 		</view>
@@ -99,28 +101,37 @@ export default {
 	data() {
 		return {
 			amount:'0.00',
-			note:''
+			note:'',
+			accounts:['微信零钱','支付宝余额','支付宝余额宝','微信零钱通'],
+			index:0
 		};
 	},
 	
 	methods: {
-		
+		bindAccountChange(e){
+			this.index = e.target.value
+			console.log(this.account)
+		},
 		submit(){
+			if(this.amount.indexOf('-')!=-1||this.amount.indexOf('+')!=-1){
+				this.getEquals()
+			}
 			console.log(this.amount)
 			
 			//将amount设置为默认值0.00
-			this.amount = '0.00'
+			// this.amount = '0.00'
 		},
 		/**
 		 * 获取键盘值
 		 * @param {Object} event
 		 */
 		getValue(event){
-			let value = event.currentTarget.dataset.num
+			let value = event.currentTarget.dataset.num.toString()
 			if(this.amount==='0.00'){
 				this.amount = value
 				return
 			}
+			console.log(this.amount,value)
 			this.amount = this.amount+value
 		},
 		/**
@@ -137,6 +148,41 @@ export default {
 			if(this.amount===''){
 				this.amount='0.00'
 			}
+		},
+		getEquals(){
+			let num1=''
+			let num2=''
+			let operator='+'
+			let isFirstOprator=true
+			// debugger
+			for(let char of this.amount){
+				if(char==='+'||char==='-'){
+					if(isFirstOprator){
+						isFirstOprator=false
+					}else{
+						if(operator==='+'){
+							num1=(parseFloat(num1)+parseFloat(num2)).toString()
+						}else{
+							num1=(parseFloat(num1)-parseFloat(num2)).toString()
+						}
+						num2=''
+					}
+					operator=char
+				}else{
+					if(isFirstOprator){
+						num1=num1+char
+					}else{
+						num2=num2+char
+					}
+				}
+				
+			}
+			if(operator==='+'){
+				this.amount=(parseFloat(num1)+parseFloat(num2)).toString()
+			}else{
+				this.amount=(parseFloat(num1)-parseFloat(num2)).toString()
+			}
+			console.log(this.amount)
 		}
 	}
 };
@@ -227,26 +273,23 @@ export default {
 	display: flex;
 	flex-direction: row;
 	align-items: center;
-	width: 20%;
+	width: 30%;
 	padding: 0 0 0 20upx;
 }
 
 .keybord-top .note .icon{
-	height: 30upx;
-	width: 30upx;
+	height: 40upx;
+	width: 40upx;
 }
 
 .keybord-top .note .text{
 	margin: 0 0 0 16upx;
 }
 
-.keybord-top .note-input{
-	width: 50%;
-}
-
 .keybord-top .amount{
-	width: 30%;
-	text-align: center;
+	width: 70%;
+	text-align: right;
+	margin: 0 10upx 0 0;
 }
 
 .keybord-content{
@@ -257,6 +300,11 @@ export default {
 	width: 100%;
 	height: 85%;
 	margin: 15upx 0 0 0;
+}
+
+.keybord-content .icon{
+	height: 50upx;
+	width: 50upx;
 }
 
 .keybord-content .item{
